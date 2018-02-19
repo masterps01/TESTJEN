785b333965d6fbe245c593b335a064d146d46583
masterps01 <amahatoey@gmail.com>
2018-02-19 13:23:11 +0700

    Round 1

---
+++ Test Suites/TESTJEN.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TESTJEN</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>6ebb3f5e-94ad-405d-a4c2-8660ff00f75b</testSuiteGuid>
+</TestSuiteEntity>
